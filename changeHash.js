da4967060d10d4ce504fb2dc88898f69df5a6156
Jacob Wilson <jwilson1233@gmail.com>
2023-11-27 04:18:26 -0600

    added error checking

--- changeHash.js
+++ changeHash.js
@@ -4,20 +4,32 @@ const { readdirSync } = require('node:fs');
 const { createHash } = require('node:crypto');
 const { randomBytes } = require('node:crypto');
 const { Buffer } = require('node:buffer');
-const targetDirectory = __dirname + process.argv[2].toString(); // requires a directory
 
-readdirSync(targetDirectory).forEach(name => {
-    let hashBefore = createHash('sha256');
-    let hashAfter = createHash('sha256');
-    let file = readFileSync(`${__dirname + process.argv[2].toString()}/${name}`);
-    let newFile = appendBytesToBuffer(file);
-    writeFileSync(`${__dirname + process.argv[2].toString()}/${name}`, newFile);
-    hashBefore.update(file);
-    hashAfter.update(newFile);
-    console.log(`\n${name}`);
-    console.log(`Before: ${hashBefore.digest('hex')}`);
-    console.log(`After: ${hashAfter.digest('hex')}`);
-});
+if (process.argv[2]) {
+    const targetDirectory = `${__dirname}/${process.argv[2].toString()}`;
+    if (targetDirectory.slice(-1).localeCompare('/') < 1) {
+        try {
+            readdirSync(targetDirectory).forEach(fileName => {
+                let hashBefore = createHash('sha256');
+                let hashAfter = createHash('sha256');
+                let file = readFileSync(targetDirectory + fileName);
+                let newFile = appendBytesToBuffer(file);
+                writeFileSync(targetDirectory + fileName, newFile);
+                hashBefore.update(file);
+                hashAfter.update(newFile);
+                console.log(`\n${fileName}`);
+                console.log(`Old SHA256: ${hashBefore.digest('hex')}`);
+                console.log(`New SHA256: ${hashAfter.digest('hex')}`);
+            });
+        } catch (err) {
+            console.log("Please make sure your path is relative and correct");
+        }
+    } else {
+        console.log("Please make sure your path ends in /");
+    }
+} else {
+    console.log("Please make sure you include a path");
+}
 
 function appendBytesToBuffer(data) {
     let rand = randomBytes(2);
